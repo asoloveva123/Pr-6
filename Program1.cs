f1b714d469ca948ef20dcfce5d4f0cbbe0cf1120
Настя Соловьева <121259111+asoloveva123@users.noreply.github.com>
2022-12-24 21:56:16 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("a= ");
+            int a = int.Parse(Console.ReadLine()); //ввод первого числа с клавиатуры 
+            Console.Write("b= ");
+            int b = int.Parse(Console.ReadLine()); //ввод второго числа с клавиатуры
+            Console.WriteLine("{0}+{1}={2}", a, b, a + b); //вычисление суммы введеных чисел и вывод результата на экран
+        }
+    }
+}
---
+++ Program3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            Console.Write("a= "); //ввод первого числа с клавиатуры
+            a = Convert.ToInt16(Console.ReadLine());
+            Console.Write("b= "); //ввод второго числа с клавиатуры
+            b = Convert.ToInt16(Console.ReadLine());
+            Console.Write("c= "); //ввод третьего числа с клавиатуры
+            c = Convert.ToInt16(Console.ReadLine());
+            {
+                sum = a + b + c;
+            }
+            Console.WriteLine("{0}+{1}+{2} = {3}", a, b, c, a + b + c); //вычисление суммы введенных чисел и вывод результата на экран 
+        }
+    }
+}
