cd14c4b4666f8ab985de7ded2c2a383a7134fe10
Настя Соловьева <121259111+asoloveva123@users.noreply.github.com>
2022-12-24 22:16:49 +0300

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double a = 0;
+            double b = 0;
+            double c = 0;
+            Console.Write("a= "); 
+            a = Convert.ToDouble(Console.ReadLine()); //ввод первого числа с клавиатуры
+            Console.Write("b= "); 
+            b = Convert.ToDouble(Console.ReadLine()); //ввод второго числа с клавиатуры
+            {
+                sum = a * b;
+            }
+            Console.WriteLine("{0}*{1} = {2}", a, b, a * b); //вычисление умножения введенных чисел и вывод результата на экран 
+            Console.WriteLine("{0:f1}", Math.Round(sum, 1));
+        }
+    }
+}
---
+++ Program5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double a = 0;
+            double b = 0;
+            Console.Write("a= ");
+            a = Convert.ToDouble(Console.ReadLine()); //ввод первого числа с клавиатуры
+            Console.Write("b= ");
+            b = Convert.ToDouble(Console.ReadLine()); //ввод второго числа с клавиатуры
+            {
+                sum = a / b;
+            }
+            Console.WriteLine("{0}/{1}", a, b); 
+            Console.WriteLine("{0}", Math.Round(sum, 3)); //вычисление деления введенных чисел и вывод результата на экран 
+            Console.ReadLine();
+        }
+    }
+}
